ae97f840a59898e300a4c7c4e0cea4ad98d686ae
IngNex <ingnexmaicol14@gmail.com>
2022-09-27 11:32:14 -0500

    added data dbz img

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,32 +13,124 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  /* ================ Consumir Api ================= */
+  var DBZApi =
+      'https://raw.githubusercontent.com/IngNex/dragonball-api-sagas/master/dragonballz.json';
+
+  List dbzData = [];
+
+  void initState() {
+    super.initState();
+    if (mounted) {
+      fetchDadronBallData();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.all(Radius.circular(30))),
-          child: Stack(
+      body: Stack(
+        children: [
+          Positioned(
+              child: Column(
             children: [
-              Positioned(
-                bottom: -25,
-                right: -25,
-                child: Image.asset(
-                  'images/logo.png',
-                  height: 150,
-                  fit: BoxFit.fitWidth,
-                  color: Colors.white12.withOpacity(0.2),
-                  colorBlendMode: BlendMode.modulate,
-                ),
-              ),
+              dbzData != null
+                  ? Expanded(
+                      child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2, childAspectRatio: 1.4),
+                          itemCount: dbzData.length,
+                          itemBuilder: (context, index) {
+                            var img = dbzData[index]['img'];
+                            var name = dbzData[index]['name'];
+                            return InkWell(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 6, horizontal: 10),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.amber,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(30))),
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                        child: Center(
+                                          widthFactor: 1,
+                                          child: Container(
+                                            width: 130,
+                                            height: 130,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(100),
+                                                border: Border.all(
+                                                    color: Colors.white,
+                                                    width: 2)),
+                                            child: ClipOval(
+                                              child: CachedNetworkImage(
+                                                imageUrl: img,
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        bottom: 10,
+                                        left: width / 100 * 15,
+                                        child: Container(
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                top: 4,
+                                                bottom: 4,
+                                                left: 8,
+                                                right: 8),
+                                            child: Text(name.toString()),
+                                          ),
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(20),
+                                              ),
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
+                    )
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    )
             ],
-          ),
-        ),
+          ))
+        ],
       ),
     );
+  }
+
+  void fetchDadronBallData() async {
+    var url = Uri.https(
+        "raw.githubusercontent.com",
+        "/IngNex/dragonball-api-sagas/master/dragonballz.json",
+        {'q': '{http}'});
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var decodedJsonData = jsonDecode(response.body);
+      dbzData = decodedJsonData['personages'];
+      /*int i = 0;
+      while (i < 10) {
+        print('${dbzData[i]['name']}');
+        i++;
+      }*/
+      setState(() {});
+    }
   }
 }
